0e70fc83c4c436f8447ec1338209fc066a809a37
leverage2014 <myletter428@gmail.com>
2016-07-01 11:38:46 -0400

    update port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var middleware = require('./middleware');
 // 	res.send('Hello, express');
 // });
 
-var PORT = 3000 || process.env.PORT; 
+var PORT = process.env.PORT || 3000; 
 
 //app.use(middleware.requireAuthentication);   // middleware on the top
 
