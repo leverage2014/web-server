40133e1908fc4246daba600703b3c75160b6e976
leverage2014 <myletter428@gmail.com>
2016-07-01 10:11:16 -0400

    initialize repo

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+
+var middleware = {
+	requireAuthentication: function(req, res, next){
+		console.log('private route hit!');
+		next();
+	},
+
+	logger: function(req, res, next){
+		console.log(req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+// app.get('/', function(req, res){
+// 	res.send('Hello, express');
+// });
+
+var PORT = 3000; 
+
+//app.use(middleware.requireAuthentication);   // middleware on the top
+
+// app.get('/about', function(req, res){
+// 	res.send('about us');
+// });
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('about us');
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT, function(){
+	console.log('Express server started on port ' + PORT +' !');
+});
+
